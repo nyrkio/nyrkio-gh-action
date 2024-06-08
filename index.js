010435a3e615c19b0365b727f7153f730a6958bf
Matt Fleming <matt@nyrk.io>
2024-06-08 10:19:11 +0100

    Initial commit for GH action

---
+++ index.js
@@ -0,0 +1,21 @@
+// Copyright (C) 2024 Nyrkiö Oy
+const fs = require("fs");
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  const filename = core.getInput("filename");
+  fs.promises.readFile(filename, "utf8").then((data) => {
+    console.log(data);
+  });
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput("who-to-greet");
+  console.log(`Hello ${nameToGreet}!`);
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
